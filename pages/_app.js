de19365ee86e8a14f323ac9407baf4d3613710c1
Eric Whale <ericwhale@Erics-MBP.local>
2021-12-29 17:22:02 +0900

    compile error patch#2

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
     <div>
       <Head>
         @import
-        url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+        url(&apos;https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap@apos;);
       </Head>
       <div>
         <Nav />
