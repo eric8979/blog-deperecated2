0875e0fd4bfae5721eeab6105aec0581741f6473
Eric Whale <ericwhale@Erics-MBP.local>
2021-12-29 11:13:50 +0900

    mobile design done

--- components/footer.js
+++ components/footer.js
@@ -5,38 +5,41 @@ const Footer = () => {
   return (
     <>
       <div className={styles.footer}>
-        <div className={styles.block}>© 2021, Eric Whale</div>
-        <div className={styles.block}>
-          <Link href="https://github.com/eric8979">GitHub</Link>
+        <div className={styles.box}>
+          <div className={styles.block}>© 2021, Eric Whale</div>
+          <div className={styles.block}>
+            <Link href="https://github.com/eric8979">GitHub</Link>
+          </div>
+          <div className={styles.block}>
+            <Link href="https://www.linkedin.com/in/eric-whale-4853301ab/">
+              LinkedIn
+            </Link>
+          </div>
+          <div className={styles.block}>
+            <Link href="https://twitter.com/ericthewhale">@ericthewhale</Link>
+          </div>
         </div>
-        <div className={styles.block}>
-          <Link href="https://www.linkedin.com/in/eric-whale-4853301ab/">
-            LinkedIn
-          </Link>
-        </div>
-        <div className={styles.block}>
-          <Link href="https://twitter.com/ericthewhale">@ericthewhale</Link>
-        </div>
-        <div className={styles.block}>hsh048148@gmail.com</div>
-        <div className={styles.lastblock}>
-          <a
-            rel="license"
-            href="http://creativecommons.org/licenses/by-nc-nd/4.0/"
-          >
-            <img
-              alt="Creative Commons License"
-              src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png"
-            />
-          </a>
-          <br />
-          {/* This work is licensed under a{" "} */}
-          <a
-            rel="license"
-            href="http://creativecommons.org/licenses/by-nc-nd/4.0/"
-          >
-            {/* Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 */}
-            {/* International License */}
-          </a>
+        <div className={styles.box2}>
+          <div className={styles.block}>hsh048148@gmail.com</div>
+          <div className={styles.lastblock}>
+            <a
+              rel="license"
+              href="http://creativecommons.org/licenses/by-nc-nd/4.0/"
+            >
+              <img
+                alt="Creative Commons License"
+                src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png"
+              />
+            </a>
+            {/* This work is licensed under a{" "} */}
+            <a
+              rel="license"
+              href="http://creativecommons.org/licenses/by-nc-nd/4.0/"
+            >
+              {/* Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 */}
+              {/* International License */}
+            </a>
+          </div>
         </div>
       </div>
     </>
